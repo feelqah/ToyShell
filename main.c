c38ebffa2de7277a0f0bffff6ca20b6ef0fb4631
Mili Mil <fmaric.os@gmail.com>
2020-08-19 16:40:22 +0200

    Add split line function

--- main.c
+++ main.c
@@ -1,14 +1,47 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
 
 #define TS_RL_BUFSIZE 1024
+#define TS_TOK_BUFSIZE 64
+#define TS_TOK_DELIM " \t\r\n\a"
 
 int ts_execute(char **args){
 	//TODO
 }
 
 char **ts_split_line(char *line){
-	//TODO
+	int bufsize = TS_TOK_BUFSIZE;
+	int position = 0;
+	char **tokens = malloc(bufsize * sizeof(char*));
+	char *token;
+
+	if(!tokens){
+		fprintf(stderr, "ts: allocation error\n");
+		exit(EXIT_FAILURE);
+	}
+
+	token = strtok(line, TS_TOK_DELIM);
+	while(token != NULL){
+		tokens[position] = token;
+		position++;
+
+		if(position >= bufsize){
+			bufsize += TS_TOK_BUFSIZE;
+			tokens = realloc(tokens, bufsize * sizeof(char*));
+
+			if(!tokens){
+				fprintf(stderr, "ts: allocation error\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		token = strtok(NULL, TS_TOK_DELIM);
+	}
+	tokens[position] = NULL;
+	return tokens;
 }
 
 char *ts_read_line(void){
@@ -70,6 +103,7 @@ int main(int argc, char **argv){
 
   // command loop
   ts_loop();
+
   // cleanup
 
   return EXIT_SUCCESS;
