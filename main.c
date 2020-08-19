d92ee42dbe5e6fbf9ac8c962d49afa176fe570fb
Mili Mil <fmaric.os@gmail.com>
2020-08-19 12:39:41 +0200

    Add main function and stub for loop function

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+
+void ts_loop(){
+  //TODO
+}
+
+int main(int argc, char **argv){
+  // load config files
+
+  // command loop
+  ts_loop();
+  // cleanup
+
+  return EXIT_SUCCESS;
+}
